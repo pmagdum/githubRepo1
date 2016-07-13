21ddd66149b617a0c335b93485b8039ae6fc2c07
pmagdum <pmagdum@gmail.com>
2016-07-13 14:57:02 +0530

    Update GithubClass.java

--- src/githubRepoPkg/GithubClass.java
+++ src/githubRepoPkg/GithubClass.java
@@ -6,6 +6,8 @@ public class GithubClass {
 	{
 		System.out.println("This is github demo....");
 		System.out.println("From eclips to github..");
+		System.out.println("From github to eclips....");
+		System.out.println("Checking out.....");
 
 	}
 
