5d911c66e7e65e009a906f17fdf0963f0bdab255
pmagdum <pmagdum@gmail.com>
2016-07-13 14:55:15 +0530

    loading githubProject first time....

---
+++ src/githubRepoPkg/GithubClass.java
@@ -0,0 +1,12 @@
+package githubRepoPkg;
+
+public class GithubClass {
+
+	public static void main(String[] args) 
+	{
+		System.out.println("This is github demo....");
+		System.out.println("From eclips to github..");
+
+	}
+
+}
